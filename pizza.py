9d4978b588be95b41cf0b9133507d459c1f0f1f1
Gamerz <136964406+GamerzSocks@users.noreply.github.com>
2023-12-04 12:19:46 +0800

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,5 +1,8 @@
 # pizza virus
+
+#Global imports
 import time
+from datetime import datetime, timedelta
 
 print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⣄⣄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀")
 print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠊⠉⠉⠉⠉⠉⣩⡷⠋⠉⠹⡛⢽⠢⣀⣀⡀⠀")
@@ -16,7 +19,7 @@ print(" Domino's Windows App!")
 print("")
 print("")
 print("-----------------------------")
-#just to break up my spaghetti code
+# just to break up my spaghetti code
 
 
 
@@ -29,16 +32,42 @@ print("Hawaiian Pizza: A classic pairing of smoky leg ham & sweet pineapple piec
 print("")
 pizza_selection = input("What pizza would you like to order? (NAME OF PIZZA eg. Pepperoni): ")
 time.sleep(2)
-
+print("")
 print("Okay, 1 large " + pizza_selection + " pizza!")
 time.sleep(2)
 
-print("Please insert a Delivery Address:")
-DeliveryAddress = input()
 
+DeliveryAddress = input("Please insert a Delivery Address: ")
+
+print("")
 print("Ok, Delivering to " + DeliveryAddress + " !")
 time.sleep(10)
+print("")
 print("We have recieved your order. We'll begin your live tracking ASAP.")
+# "live" tracker
 time.sleep(1)
-print("Connecting to Store...)
+print("")
+print("Connecting to Store...")
+print("")
+time.sleep(5)
+print("Recieved!")
+print("")
+print("We are making your pizza right now...")
+time.sleep(50)
+print("")
+print("Putting your pizza in the oven...")
+time.sleep(100)
+print("")
+print("Boxing it up...")
+time.sleep(15)
+print("")
+print("Beginning delivery to " + DeliveryAddress + ".")
+
+# ETA Calculations
+CT = datetime.now()
+ETA = CT + timedelta(minutes=5)
+STRETA = ETA.strftime("%H:%M:%S")
+
+print("Estimated Arrival Time: " + STRETA)
+
 
